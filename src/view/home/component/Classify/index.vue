7f325585134de71471c602b57515631581df3b76
djj <2861606659@qq.com>
2023-06-16 18:21:45 +0800

    饼图

--- src/view/home/component/Classify/index.vue
+++ src/view/home/component/Classify/index.vue
@@ -3,31 +3,183 @@
  * @Date: 2023-06-14 23:42:07
  * @FilePath: \DjjBlogs\src\view\home\component\Classify\index.vue
  * @LastEditors: djj
- * @LastEditTime: 2023-06-15 00:10:49
+ * @LastEditTime: 2023-06-16 01:09:00
 -->
 <template>
-    <el-card class="box-card">
-        <template #header>
-          <div class="card-header row-between-center">
-            <span>分类销量排行</span>
-            <el-radio-group v-model="radio1" class="view-radio-group">
-            <el-radio-button label="品类" />
-            <el-radio-button label="商品" />
-          </el-radio-group>
-          </div>
-        </template>
-        <div v-for="o in 4" :key="o" class="text item">{{ "List item " + o }}</div>
-      </el-card>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header row-between-center">
+        <span>分类销量排行</span>
+        <el-radio-group v-model="radio1" class="view-radio-group">
+          <el-radio-button label="品类" />
+          <el-radio-button label="商品" />
+        </el-radio-group>
+      </div>
+    </template>
+    <div class="search-view-wapper">
+      <div id="classify-echart" :style="{ width: '100%', height: '100%' }"></div>
+    </div>
+  </el-card>
 </template>
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref, onMounted } from "vue";
+import { EChartsOption } from "echarts";
+import { nodeElemt } from "@/hooks/echatesHook";
+import { type } from "os";
 const radio1 = ref("品类");
+type ParamsType = {
+  legendname: string;
+  value: number;
+  percent: string;
+  name: string;
+};
+
+type FormatterParamsType = {
+  componentType: "series";
+  // 系列类型
+  seriesType: string;
+  // 系列在传入的 option.series 中的 index
+  seriesIndex: number;
+  // 系列名称
+  seriesName: string;
+  // 数据名，类目名
+  name: string;
+  // 数据在传入的 data 数组中的 index
+  dataIndex: number;
+  // 传入的原始数据项
+  data: Object;
+  // 传入的数据值。在多数系列下它和 data 相同。在一些系列下是 data 中的分量（如 map、radar 中）
+  value: number | Array<any> | Object;
+  // 坐标轴 encode 映射信息，
+  // key 为坐标轴（如 'x' 'y' 'radius' 'angle' 等）
+  // value 必然为数组，不会为 null/undefied，表示 dimension index 。
+  // 其内容如：
+  // {
+  //     x: [2] // dimension index 为 2 的数据映射到 x 轴
+  //     y: [0] // dimension index 为 0 的数据映射到 y 轴
+  // }
+  encode: Object;
+  // 维度名列表
+  dimensionNames: Array<String>;
+  // 数据的维度 index，如 0 或 1 或 2 ...
+  // 仅在雷达图中使用。
+  dimensionIndex: number;
+  // 数据图形的颜色
+  color: string;
+  // 饼图/漏斗图的百分比
+  percent: number;
+  // 旭日图中当前节点的祖先节点（包括自身）
+  treePathInfo: Array<any>;
+  // 树图/矩形树图中当前节点的祖先节点（包括自身）
+  treeAncestors: Array<any>;
+};
+const mockData = [
+  {
+    legendname: "粉面粥店",
+    value: 67,
+    percent: "15.40%",
+    name: "粉面粥店|15.40%",
+  },
+  {
+    legendname: "快餐",
+    value: 97,
+    percent: "22.40%",
+    name: "快餐|22.40%",
+  },
+  {
+    legendname: "汉堡炸鸡",
+    value: 92,
+    percent: "21.15%",
+    name: "汉堡炸鸡|21.15%",
+  },
+];
+function renderPieChart(mockData: Array<any>): EChartsOption {
+  return {
+    title: [
+      {
+        text: "品类分布",
+        textStyle: {
+          fontSize: 14,
+          color: "#666",
+        },
+        left: 20,
+        top: 20,
+      },
+      {
+        text: "累计订单量",
+        subtext: "320",
+        left: "34.5%",
+        top: "42.5%",
+        textStyle: {
+          fontSize: 14,
+        },
+        subtextStyle: {
+          fontSize: 28,
+        },
+        textAlign: "center",
+      },
+    ],
+    tooltip: {
+      show: true,
+      trigger: "item",
+      formatter: function (params) {
+        console.log("params", params);
+
+        const data = params as any;
+        return `${data.seriesName}<br/> ${data.marker}${data.data.legendname}<br/>数量: ${data.data.value}<br/>占比:${data.data.percent}`;
+      },
+    },
+    series: [
+      {
+        name: "品类分布",
+        type: "pie",
+        data: mockData,
+        label: {
+          show: true,
+          position: "outer",
+          formatter: function (params) {
+            const data = params.data as ParamsType;
+            return `${data.legendname}`;
+          },
+        },
+        itemStyle: {
+          borderWidth: 4,
+          borderColor: "#fff",
+        },
+        center: ["35%", "50%"],
+        radius: ["45%", "60%"],
+        labelLine: {
+          length: 5,
+          length2: 3,
+        },
+        clockwise: true,
+      },
+    ],
+    legend: {
+      type: "scroll",
+      orient: "vertical",
+      height: 250,
+      left: "70%",
+      top: "middle",
+      textStyle: {},
+    },
+  };
+}
+
+onMounted(() => {
+  nodeElemt("#classify-echart", renderPieChart(mockData));
+});
 </script>
 
 <style lang="scss" scoped>
-.card-header{
-    .view-radio-group{
-        text-align: right;
-    }
+.card-header {
+  .view-radio-group {
+    text-align: right;
+  }
+}
+.search-view-wapper {
+  height: 452px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/view/home/component/Search/index.vue
+++ src/view/home/component/Search/index.vue
@@ -3,7 +3,7 @@
  * @Date: 2023-06-14 23:40:50
  * @FilePath: \DjjBlogs\src\view\home\component\Search\index.vue
  * @LastEditors: djj
- * @LastEditTime: 2023-06-15 00:01:36
+ * @LastEditTime: 2023-06-15 23:17:21
 -->
 <template>
   <el-card class="box-card">
@@ -17,12 +17,12 @@
         <div class="view-item">
           <div>搜索用户数</div>
           <div>93,634</div>
-          <div></div>
+          <div id="search-echart" :style="{ width: '100%', height: '100%' }"></div>
         </div>
         <div class="view-item">
           <div>搜索量</div>
           <div>198,782</div>
-          <div></div>
+          <div id="ranking-echart" :style="{ width: '100%', height: '100%' }"></div>
         </div>
       </div>
       <div class="search-view-item2">
@@ -32,13 +32,16 @@
           <el-table-column prop="searchVolume" label="搜索总量"/>
           <el-table-column prop="searchUser" label="搜索用户数" />
         </el-table>
-        <el-pagination background layout="prev, pager, next" :total="1000" />
+        <el-pagination class="pagination" background layout="prev, pager, next" :total="1000" />
       </div>
     </div>
   </el-card>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
+import {EChartsOption} from "echarts";
+import { nodeElemt } from "@/hooks/echatesHook";
 const tableData = [
   {
     ranking: "1",
@@ -65,12 +68,66 @@ const tableData = [
     searchUser:"7888"
   },
 ];
+const arr1=[220,215,5,190,180,56,110,80,10,130,120,120,60,90,80,10,8,7,6,5,0];
+const arr2=[562,443,553,236,52,145,635,123,423,326,254,261,259,225,250,350,420,125,330,540,220]
+function setOption(seriesData:number[]):EChartsOption{
+  return {
+    xAxis:{
+      type:"category",
+      show:false,
+      boundaryGap:false
+    },
+    yAxis:{
+      show:false,
+    },
+    series:[
+      {
+        type:"line",
+        data:seriesData,
+        areaStyle:{
+          color:"rgba(95,187,255,.5)",
+        },
+        lineStyle:{
+          width:0
+        },
+        itemStyle:{
+          opacity:0
+        },
+        smooth:true
+      }
+    ],
+    grid:{
+      top:0,
+      right:0,
+      left:0,
+      bottom:0
+
+    }
+  }
+}
+
+onMounted(()=>{
+  nodeElemt("#search-echart", setOption(arr1));
+  nodeElemt("#ranking-echart", setOption(arr2));
+})
 </script>
 <style lang="scss" scoped>
 .search-view-wapper {
+  display: flex;
+  flex-direction: column;
+  height: 452px;
   .search-view-item1 {
     .view-item {
       flex: 1;
+      padding: 0 10px;
+    }
+  }
+  .search-view-item2{
+    margin-top: 20px;
+    .pagination{
+      margin-top: 20px;
+     display: flex;
+     justify-content: end;
     }
   }
 }
